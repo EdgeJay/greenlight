be074d3bf9971210b7da6842add1177e989381b2
Wu Huijie <hjwu85@gmail.com>
2017-04-13 14:49:26 +0800

    setup base app file

---
+++ index.js
@@ -0,0 +1,2 @@
+import Vue from 'vue';
+import Hello from './src/views/App.vue';
---
+++ src/views/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <span></span>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  created() {
+  },
+};
+</script>
+
+<style lang="scss" module>
+
+</style>
