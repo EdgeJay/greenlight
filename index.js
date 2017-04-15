5b391c3bbb9a437afa6d6b535f0c0e93f9d1eda6
Wu Huijie <hjwu85@gmail.com>
2017-04-16 07:48:53 +0800

    added nested views

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 import App from './src/views/App.vue';
+import HomeView from './src/views/HomeView.vue';
+import SignInView from './src/views/SignInView.vue';
 
 const routes = [
-  { path: '/', component: App },
+  { path: '/', component: App,
+    children: [
+      { path: '', component: HomeView },
+      { path: 'signin', component: SignInView },
+    ]
+  },
 ];
 
 // enable Flux-styled state management
