4580aa8f9b1c644f4aaa2d23195401b6570f891a
Wu Huijie <hjwu85@gmail.com>
2017-05-13 14:44:34 +0800

    code cleanup

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const views = require('koa-views');
-// const serve = require('koa-static');
 const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
 const webpack = require('webpack');
 const config = require('../config/webpack.dev.js');
@@ -27,10 +26,6 @@ app.use(async (ctx, next) => {
   ctx.state.jsAssetSuffix = process.env.NODE_ENV === 'production' ? '.min.js' : '.js';
   await next();
 });
-
-/* Serve static assets */
-// app.use(serve(path.resolve(__dirname, '../public')));
-// app.use(serve(path.resolve(__dirname, '../resources')));
 
 /* Template rendering using dustjs */
 app.use(views(path.resolve(__dirname, 'views'), {
