d4380e476007607fe40fe184603b2b999ada403f
Wu Huijie <hjwu85@gmail.com>
2017-05-13 15:12:49 +0800

    restored server static assets for production env

--- server/index.js
+++ server/index.js
@@ -4,20 +4,25 @@ const path = require('path');
 const Koa = require('koa');
 const Router = require('koa-router');
 const views = require('koa-views');
-const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware');
-const webpack = require('webpack');
-const config = require('../config/webpack.dev.js');
+const serve = require('koa-static');
 
 const app = new Koa();
 const router = new Router();
 
 /* Hot module replacement */
 if (process.env.NODE_ENV === 'development') {
-  const compiler = webpack(config);
+  const webpack = require('webpack'); // eslint-disable-line global-require
+  const { devMiddleware, hotMiddleware } = require('koa-webpack-middleware'); // eslint-disable-line global-require
+  const config = require('../config/webpack.dev.js'); // eslint-disable-line global-require
+  const compiler = webpack(config); // eslint-disable-line global-require
+
   app.use(devMiddleware(compiler, {
     publicPath: '/assets/js/',
   }));
   app.use(hotMiddleware(compiler));
+} else if (process.env.NODE_ENV === 'production') {
+  app.use(serve(path.resolve(__dirname, '../public')));
+  app.use(serve(path.resolve(__dirname, '../resources')));
 }
 
 /* Set global state */
