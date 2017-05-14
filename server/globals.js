65da9e9bb5807b32f2684fda392ca7545c78c9d8
Wu Huijie <hjwu85@gmail.com>
2017-05-14 22:59:59 +0800

    added google client id

--- server/globals.js
+++ server/globals.js
@@ -2,6 +2,12 @@ module.exports = (app) => {
   app.use(async (ctx, next) => {
     ctx.state.assetServer = process.env.NODE_ENV === 'production' ? '' : '//localhost:9090';
     ctx.state.jsAssetSuffix = process.env.NODE_ENV === 'production' ? '.min.js' : '.js';
+    ctx.state.googleClientId = process.env.GOOGLE_CLIENT_ID;
+
+    if (!ctx.state.googleClientId) {
+      throw new Error('Environment variable not provided! Make sure you added your own GOOGLE_CLIENT_ID variable to .env file');
+    }
+
     await next();
   });
 };
